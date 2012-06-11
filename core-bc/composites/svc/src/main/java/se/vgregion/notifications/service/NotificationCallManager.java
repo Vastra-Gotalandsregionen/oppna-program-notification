f67ba7dff973f3ce99eac1d462c21d6ba17b3908
patrik.bergstrom <patrik.bergstrom@knowit.se>
2012-06-11 11:56:31 +0000

    Decreased logging. Bugfix with the right thread-safe Map implementation.

--- core-bc/composites/svc/src/main/java/se/vgregion/notifications/service/NotificationCallManager.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/notifications/service/NotificationCallManager.java
@@ -25,14 +25,13 @@ public class NotificationCallManager {
     @Value("${notification.banned.services}")
     private String bannedServicesString;
 
-    private Map<String, Integer> consecutiveNullsMap = new ConcurrentHashMap<String, Integer>();
-    private Map<String, Date> earliestAllowedDates = new ConcurrentHashMap<String, Date>();
+    // Represents the number of consecutive null responses in a row for a specified key (serviceName + screenName)
+    private ConcurrentHashMap<String, Integer> consecutiveNullsMap = new ConcurrentHashMap<String, Integer>();
+    private ConcurrentHashMap<String, Date> earliestAllowedDates = new ConcurrentHashMap<String, Date>();
     private Set<String> bannedServices = Collections.synchronizedSet(new HashSet<String>());
 
     // This Queue is for deciding when the two Maps should be cleared of elements.
     private ConcurrentLinkedQueue<String> currentKeysInCache = new ConcurrentLinkedQueue<String>();
-
-    private Object lock = new Object();
 
     public NotificationCallManager() {
 
@@ -51,61 +50,59 @@ public class NotificationCallManager {
     public void notifyValue(String serviceName, Integer value, String screenName) {
         String key = getKey(serviceName, screenName);
 
-        synchronized (lock) {
-            if (value == null) {
-                Integer numberNull = consecutiveNullsMap.get(key);
-                if (numberNull == null) {
-                    numberNull = 1; // Null for first time
-                    consecutiveNullsMap.put(key, numberNull);
-                    currentKeysInCache.add(key);
+        if (value == null) {
+            Integer numberNull = consecutiveNullsMap.get(key);
+            if (numberNull == null) {
+                numberNull = 1; // Null for first time
+                consecutiveNullsMap.put(key, numberNull);
+                currentKeysInCache.add(key);
 
-                    // Remove elements if there are to many
-                    while (currentKeysInCache.size() > 10000) {
-                        String keyToRemove = currentKeysInCache.poll();
-                        // Remove from the two maps
-                        consecutiveNullsMap.remove(keyToRemove);
-                        earliestAllowedDates.remove(keyToRemove);
-                    }
-                    LOGGER.debug(getKey(key, " has resulted in null value for the first time."));
-                } else {
-                    numberNull++;
-                    consecutiveNullsMap.put(key, numberNull);
-                    LOGGER.debug(key + " has resulted in null " + numberNull + " times.");
+                // Remove elements if there are to many
+                while (currentKeysInCache.size() > 10000) {
+                    String keyToRemove = currentKeysInCache.poll();
+                    // Remove from the two maps
+                    consecutiveNullsMap.remove(keyToRemove);
+                    earliestAllowedDates.remove(keyToRemove);
                 }
-                double minutesDelayToNextCheck = Math.pow(2, numberNull); // 2, 4, 8, 16, 32, 64, 128, 256, 512
-
-                if (minutesDelayToNextCheck > 256) {
-                    minutesDelayToNextCheck = 256;
-                }
-
-                Calendar earliestAllowedDateToCheckAgain = Calendar.getInstance();
-                earliestAllowedDateToCheckAgain.add(Calendar.MINUTE, (int) minutesDelayToNextCheck);
-
-                earliestAllowedDates.put(key, earliestAllowedDateToCheckAgain.getTime());
+                LOGGER.warn(key + " has resulted in null value for the first time.");
             } else {
-                consecutiveNullsMap.remove(key); // Reset - zero consecutive nulls
-                earliestAllowedDates.remove(key); // No date limit
+                numberNull++;
+                consecutiveNullsMap.put(key, numberNull);
+                LOGGER.warn(key + " has resulted in null " + numberNull + " times.");
             }
+            double minutesDelayToNextCheck = Math.pow(2, numberNull); // 2, 4, 8, 16, 32, 64, 128, 256, 512
+
+            if (minutesDelayToNextCheck > 256) {
+                minutesDelayToNextCheck = 256;
+            }
+
+            Calendar earliestAllowedDateToCheckAgain = Calendar.getInstance();
+            earliestAllowedDateToCheckAgain.add(Calendar.MINUTE, (int) minutesDelayToNextCheck);
+
+            earliestAllowedDates.put(key, earliestAllowedDateToCheckAgain.getTime());
+        } else {
+            consecutiveNullsMap.remove(key); // Reset - zero consecutive nulls
+            earliestAllowedDates.remove(key); // No date limit
         }
     }
 
     public boolean shouldICallThisService(String serviceName, String screenName) {
         if (bannedServices.contains(serviceName)) {
-            LOGGER.debug(serviceName + " is banned. Do not call.");
+            LOGGER.trace(serviceName + " is banned. Do not call.");
             return false;
         }
 
         String key = getKey(serviceName, screenName);
         Date date = earliestAllowedDates.get(key);
         if (date == null) {
-            LOGGER.info(key + " should result in a call.");
+            LOGGER.trace(key + " should result in a call.");
             return true; // No date limit for this key
         } else {
             if (date.after(new Date())) {
-                LOGGER.info(key + " should NOT result in a call.");
+                LOGGER.trace(key + " should NOT result in a call.");
                 return false; // We're not there yet
             } else {
-                LOGGER.info(key + " should result in a call.");
+                LOGGER.trace(key + " should result in a call.");
                 return true;
             }
         }
@@ -132,11 +129,9 @@ public class NotificationCallManager {
 
     @ManagedOperation
     public void resetState() {
-        synchronized (lock) {
-            consecutiveNullsMap = new HashMap<String, Integer>();
-            earliestAllowedDates = new HashMap<String, Date>();
-            currentKeysInCache = new ConcurrentLinkedQueue<String>();
-        }
+        consecutiveNullsMap = new ConcurrentHashMap<String, Integer>();
+        earliestAllowedDates = new ConcurrentHashMap<String, Date>();
+        currentKeysInCache = new ConcurrentLinkedQueue<String>();
     }
 
     @ManagedOperation
